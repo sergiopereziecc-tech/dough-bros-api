ca01ed015c6502f845b5950df63d71f27f92c75a
señorMesa <sergioperez.iecc@gmail.com>
2026-01-29 13:56:55 +0000

    feat : Complete full CRUD functionality in OrderService

--- dough-bros-api/src/main/java/com/pizzadev/dough_bros_api/controller/OrderController.java
+++ dough-bros-api/src/main/java/com/pizzadev/dough_bros_api/controller/OrderController.java
@@ -2,13 +2,20 @@ package com.pizzadev.dough_bros_api.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pizzadev.dough_bros_api.model.OrderStatus;
 import com.pizzadev.dough_bros_api.model.PizzaOrder;
 import com.pizzadev.dough_bros_api.service.OrderService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -24,7 +31,27 @@ public class OrderController {
     public List<PizzaOrder> listAll() {
         return orderService.getAllOrders();
     }
+
+    @PostMapping("/api/orders")
+    public PizzaOrder submitOrder(@RequestBody PizzaOrder order) {
+        orderService.createOrder(order);
+        return order;
+    }
     
+    
+    //{id} dinamic parameter
+    //@PathVariable the id comes from the url
+    @DeleteMapping("/api/orders/{id}")
+    public void deleteOrder(@PathVariable String id){
+        orderService.deleteOrder(id);
+    }
+
+    @PutMapping("/api/orders/{id}")
+    public PizzaOrder updateOrder(@PathVariable String id, @RequestBody PizzaOrder newOrder) {
+        orderService.updateOrder(id, newOrder);
+        
+        return orderService.findById(id);
+    }
     
 
 }
--- dough-bros-api/src/main/java/com/pizzadev/dough_bros_api/service/OrderService.java
+++ dough-bros-api/src/main/java/com/pizzadev/dough_bros_api/service/OrderService.java
@@ -10,21 +10,44 @@ import com.pizzadev.dough_bros_api.model.PizzaOrder;
 
 @Service
 public class OrderService {
-    
+
     private final List<PizzaOrder> orders = new ArrayList<>();
 
-    public OrderService(){
+    public OrderService() {
         createOrder(new PizzaOrder("Perez", "Barbacoa", OrderStatus.RECEIVED, 20.99));
         createOrder(new PizzaOrder("Antonien", "Chorizo", OrderStatus.RECEIVED, 20.99));
     }
 
-
-    public List<PizzaOrder> getAllOrders(){
+    public List<PizzaOrder> getAllOrders() {
         return this.orders;
     }
 
-    public void createOrder(PizzaOrder order){
+    public void createOrder(PizzaOrder order) {
         this.orders.add(order);
 
+    }
+
+    public void deleteOrder(String id) {
+        this.orders.removeIf(order -> order.getId().equals(id));
+    }
+
+    public void updateOrder(String id, PizzaOrder newOrder){
+        PizzaOrder orderFound = findById(id);
+        if (orderFound != null) {
+            orderFound.setCustomerName(newOrder.getCustomerName());
+            orderFound.setPizzaType(newOrder.getPizzaType());
+            orderFound.setPrice(newOrder.getPrice());
+            orderFound.setStatus(newOrder.getStatus());
+        }
+
+    }
+
+    public PizzaOrder findById(String id) {
+        for (int i = 0; i < this.orders.size(); i++) {
+            if (orders.get(i).getId().equals(id)) {
+                return orders.get(i);
+            }
+        }
+        return null;
     }
 }
