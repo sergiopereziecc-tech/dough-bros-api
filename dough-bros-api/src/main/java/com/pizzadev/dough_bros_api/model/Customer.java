cfaa6a6e02f1ac0934636cbf3332f4d8351f2e61
señorMesa <sergioperez.iecc@gmail.com>
2026-02-25 14:57:36 +0000

    feat: create Customer entity with auto-increment iD

---
+++ dough-bros-api/src/main/java/com/pizzadev/dough_bros_api/model/Customer.java
@@ -0,0 +1,37 @@
+package com.pizzadev.dough_bros_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customers")
+public class Customer {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+}
