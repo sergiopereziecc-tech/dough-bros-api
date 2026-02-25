bfdca25f740eaa97d3a69a8b163c927accaf9e6e
señorMesa <sergioperez.iecc@gmail.com>
2026-02-25 18:02:08 +0000

    feat: finalize CustomerService with delete logic and exception handling

---
+++ dough-bros-api/src/main/java/com/pizzadev/dough_bros_api/service/CustomerServiceImpl.java
@@ -0,0 +1,60 @@
+package com.pizzadev.dough_bros_api.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.pizzadev.dough_bros_api.model.Customer;
+import com.pizzadev.dough_bros_api.repository.CustomerRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService{
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+        
+    }
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+        }else{
+            throw new NoSuchElementException("The customer with id: " + id + " it is not in our database");
+        }
+
+    }
+    @Override
+    public Optional<Customer> update(Long id, Customer customer) {
+        Optional<Customer> customerFound = findById(id);
+        if (customerFound.isPresent()) {
+            Customer existingCustomer = customerFound.get();
+            existingCustomer.setName(customer.getName());
+            existingCustomer.setEmail(customer.getEmail());
+            return Optional.of(customerRepository.save(existingCustomer));
+        }else{
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
+            
+    }
+
+
+    
+}
